a1f50de5a409bde1ff33dc1f644f948bc8dd3b00
Yi Ren <coderquestny@gmail.com>
2016-02-11 23:44:36 -0500

    New todo can be created and appened/show at the end of the list in the view.

--- js/todosView.js
+++ js/todosView.js
@@ -5,14 +5,20 @@ var TodosView = Backbone.View.extend({
 		if (!(options && options.model)) {			// if model didn't get passed it throws an error
 			throw new Error("No model specified");
 		}
+		this.model.on("add", this.onAddTodo, this);  	// subscribing to the add todo event and updates the view	
 	},
+	onAddTodo: function(todo){
+		var view = new TodoView({model: todo});
+		this.$el.append(view.render().$el);     // adding the new todo to the view
+	},
+
 	events: {
 		"click #addBtn": "onClickAdd"
 	},
 	onClickAdd: function(){
-		// var $newTodo = this.$("#newTodo")
-		// var todo = new Todo({description: $newTodo.val()})
-		// this.model.add(todo);
+		var $newTodo = this.$("#newTodo")					
+		var todo = new Todo({description: $newTodo.val()})   // creates a new todo
+		this.model.add(todo);																// add the new todo to the todo collection	
 	},
 	render: function(){
 		var self = this;
