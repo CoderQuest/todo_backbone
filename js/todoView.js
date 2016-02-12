b8a01eb7426f60b725a1a88322038b32da0e3689
Yi Ren <coderquestny@gmail.com>
2016-02-12 05:11:59 -0500

    Pressing the "Enter" key after text input allow the new todo to be added to the collection view.

--- js/todoView.js
+++ js/todoView.js
@@ -4,7 +4,7 @@ var TodoView = Backbone.View.extend({
 		if (!(options && options.model)) { // if model didn't get passed it throws an error
 			throw new Error("No model specified");
 		}
-		this.model.on("change", this.render, this);  // refreshes the view so .toggleClass(below) gets updated
+		this.model.on("change", this.render, this); // refreshes the view so .toggleClass(below) gets updated
 	},
 	events: {
 		"click .checkBox": "onClickCheckBox"
@@ -14,7 +14,7 @@ var TodoView = Backbone.View.extend({
 	},
 	onClickCheckBox: function() {
 		var count = parseInt($("#todoLeft").text())
-		if (this.model.get("isCompleted")) {				// toggles between true and false between todo completion. 
+		if (this.model.get("isCompleted")) { // toggles between true and false between todo completion. 
 			this.model.set("isCompleted", false);
 			$("#todoLeft").text(count + 1)
 		} else {
@@ -24,7 +24,7 @@ var TodoView = Backbone.View.extend({
 	},
 
 	render: function() {
-		
+
 		this.$el.toggleClass("completed", this.model.get("isCompleted"));
 		var template = _.template($("#todoTemplate").html());
 		var html = template(this.model.toJSON());
--- js/todosView.js
+++ js/todosView.js
@@ -1,45 +1,58 @@
 var TodosView = Backbone.View.extend({
 	initialize: function(options) {
-		if (!(options && options.model)) {			// if model didn't get passed it throws an error
+		if (!(options && options.model)) { // if model didn't get passed it throws an error
 			throw new Error("No model specified");
 		}
-		this.model.on("add", this.onAddTodo, this);  	// subscribing to the add todo event and updates the view	
+		this.model.on("add", this.onAddTodo, this); // subscribing to the add todo event and updates the view	
 	},
-	onAddTodo: function(todo){
-		var view = new TodoView({model: todo});
-		var todoLeft = this.model.where({isCompleted: false}).length
-		this.$("#todoList").append(view.render().$el);     // adding the new todo to the view
+	onAddTodo: function(todo) {
+		var view = new TodoView({
+			model: todo
+		});
+		var todoLeft = this.model.where({
+			isCompleted: false
+		}).length
+		this.$("#todoList").append(view.render().$el); // adding the new todo to the view
 		this.$("#todoLeft").text(todoLeft)
 	},
 	events: {
 		"click #addBtn": "onClickAdd",
-		// "click ul#todoList": "OnClickCount"
+		"keypress #newTodo": "onKeyPress",
+		"click #markAll": "onClickMarkAll"
 	},
-	OnClickCount: function() {
-		console.log('new to do clicked')
-		var todoLeft = this.model.where({isCompleted: false}).length
-		console.log(this.model.where({isCompleted: false}))
-		this.$("#todoLeft").text(todoLeft)
+	onKeyPress: function(e) {			// When "Enter" key is pressed new todo will be added to the list		
+		if (e.keyCode === 13) {			// 13 is the keycode for 'Enter'
+			this.onClickAdd();				
+		}
 	},
-	onClickAdd: function(){
-		var $newTodo = this.$("#newTodo")					
-		var todo = new Todo({description: $newTodo.val()})   // creates a new todo
-		this.model.add(todo);																// add the new todo to the todo collection
-		console.log()	
+	onClickMarkAll: function() {
+		this.model.map(function(todo) {			// goes through the collection and set all model's isCompleted to true
+			todo.set("isCompleted", true)
+		});
+		$("#todoLeft").text(0)						// Items left count becomes 0 since everything is marked completed. 
 	},
-	render: function(){
+	onClickAdd: function() {
+		var $newTodo = this.$("#newTodo");
+		var todo = new Todo({ // creates a new todo
+			description: $newTodo.val()
+		});
+		this.model.add(todo); // add the new todo to the todo collection	
+	},
+	render: function() {
 		var self = this;
-		this.$el.append("<h2>To Do List</h2>")
+		this.$el.append("<h2 id>To Do List</h2>")
 		this.$el.append("<input type='text' id='newTodo'></input>")
 		this.$el.append("<button id='addBtn'>Add item</button>")
 		this.$el.append("<ul id='todoList'></ul>")
 		this.model.each(function(todo) {
-			var view = new TodoView({model: todo});
+			var view = new TodoView({
+				model: todo
+			});
 			self.$el.append(view.render().$el);
 		})
 
+		this.$el.append("<div>Items left: <span id='todoLeft'> 0 </span> </div> <div id='markAll'>Mark all as complete</div>")
 		
-		this.$el.append("<div>Items left: <span id='todoLeft'> 0 </span> </div> <div>Mark all as complete</div>")
 		return this;
 	}
 });
